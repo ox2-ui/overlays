4cabb230a1304914bea12cfab081cc7468d4a23d
Lauricio Su <me@lauricio.com>
2017-02-14 18:57:44 +0700

    Bring state back

--- src/Panel/Panel.js
+++ src/Panel/Panel.js
@@ -4,7 +4,7 @@ import Backdrop from '../Backdrop';
 import styled from 'styled-components';
 
 const PanelContent = styled.div`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   right: 0;
@@ -21,10 +21,111 @@ const PanelContent = styled.div`
 /**
  * Panel Component
  */
-const Panel = ({ className, children, centered, hideShadow, zDepth, hideBackdrop, transition, isModal, maxWidth, width, height, top, left, right, bottom, isOpen, onDismiss, onSubmit }) => {
-  return (
-    <div>
-      {isOpen &&
+class Panel extends Component {
+  static propTypes = {
+    /**
+     * Bottom positioning property value in css lenght units.
+     */
+    bottom: PropTypes.string,
+    /**
+     * If set to true panel will be centered horizontaly.
+     */
+    centered: PropTypes.bool,
+    /**
+     * The content of the panel.
+     */
+    children: PropTypes.node,
+    /**
+     * The css class name of the root element.
+     */
+    className: PropTypes.string,
+    /**
+     * Height of the panel content.
+     */
+    height: PropTypes.string,
+    /**
+     * Controls whether the panel has hideBackdrop or not.
+     */
+    hideBackdrop: PropTypes.bool,
+    /**
+     * Disables the default shadow around panel.
+     */
+    hideShadow: PropTypes.bool,
+    /**
+     * Controls whether the panel can be closed by clicking on overlay.
+     */
+    isModal: PropTypes.bool,
+    /**
+     * Controls whether the panel is opened or not.
+     */
+    isOpen: PropTypes.bool.isRequired,
+    /**
+     * Left positioning property value in css lenght units
+     */
+    left: PropTypes.string,
+    /**
+     * Max width of the content
+     */
+    maxWidth: PropTypes.string,
+    /**
+     * Callback function fired when the Dialog is dismissed.
+     */
+    onDismiss: PropTypes.func,
+    /**
+     * Callback function fired when the Dialog is dismissed and action submitted.
+     */
+    onSubmit: PropTypes.func,
+    /**
+     * Right positioning property value in css lenght units
+     */
+    right: PropTypes.string,
+    /**
+     * Top positioning property value in css lenght units
+     */
+    top: PropTypes.string,
+    /**
+     * Transition style
+     */
+    transition: PropTypes.oneOf(['zoomIn', 'slideFromTop', 'slideFromBottom', 'fadeIn', 'none', 'slideFromLeft', 'slideFromRight']).isRequired,
+    /**
+     * Width of the content
+     */
+    width: PropTypes.string,
+    /**
+     * This number represents the zDepth of the panel.
+     */
+    zDepth: PropTypes.number,
+  };
+
+  state = {
+    isOpen: this.props.isOpen || false,
+  };
+
+  componentWillReceiveProps= (nextProps) => {
+    if (nextProps.isOpen !== this.state.isOpen) {
+      this.setState({ isOpen: nextProps.isOpen });
+    }
+  }
+
+  handleSubmit = (value) => {
+    if (this.props.onSubmit) {
+      this.props.onSubmit({ value });
+    }
+    this.setState({isOpen: false});
+  };
+
+  handleDissmis = () => {
+    if (this.props.onDismiss) {
+      this.props.onDismiss();
+    }
+    this.setState({isOpen: false});
+  };
+
+  render() {
+    const { className, children, centered, hideShadow, zDepth, hideBackdrop, transition, isModal, maxWidth, width, height, top, left, right, bottom } = this.props;
+
+    return (
+      <div className="pos:absolute-0" style={{zIndex: zDepth}}>
         <ReactCSSTransitionGroup
           transitionName={`transition:dialog-${transition}`}
           transitionAppear={true}
@@ -32,106 +133,32 @@ const Panel = ({ className, children, centered, hideShadow, zDepth, hideBackdrop
           transitionAppearTimeout={400}
           transitionLeaveTimeout={350}
         >
-          <PanelContent
-            key={1}
-            className={className}
-            hideShadow={hideShadow}
-            maxWidth={maxWidth}
-            width={width}
-            height={height}
-            centered={centered}
-            style={{zIndex: zDepth, top: top, left: left, bottom: bottom, right: right}}
-          >
-            { React.cloneElement(children, { onDismiss, onSubmit }) }
-          </PanelContent>
+          {this.state.isOpen &&
+            <PanelContent
+              key={1}
+              className={className}
+              hideShadow={hideShadow}
+              maxWidth={maxWidth}
+              width={width}
+              height={height}
+              centered={centered}
+              style={{top: top, left: left, bottom: bottom, right: right}}
+            >
+              { React.cloneElement(children, { onDismiss: this.handleDissmis, onSubmit: this.handleSubmit }) }
+            </PanelContent>
+          }
         </ReactCSSTransitionGroup>
-      }
-      { !hideBackdrop &&
-        <Backdrop
-          zDepth={zDepth - 1}
-          isOpen={isOpen}
-          onClick={isModal ? null : onDismiss}
-        />
-      }
-    </div>
-  );
-};
+        { !hideBackdrop &&
+          <Backdrop
+            isOpen={this.state.isOpen}
+            onClick={isModal ? null : this.handleDissmis}
+          />
+        }
+      </div>
+    );
+  }
+}
 
-Panel.propTypes = {
-  /**
-   * Bottom positioning property value in css lenght units.
-   */
-  bottom: PropTypes.string,
-  /**
-   * If set to true panel will be centered horizontaly.
-   */
-  centered: PropTypes.bool,
-  /**
-   * The content of the panel.
-   */
-  children: PropTypes.node,
-  /**
-   * The css class name of the root element.
-   */
-  className: PropTypes.string,
-  /**
-   * Height of the panel content.
-   */
-  height: PropTypes.string,
-  /**
-   * Controls whether the panel has hideBackdrop or not.
-   */
-  hideBackdrop: PropTypes.bool,
-  /**
-   * Disables the default shadow around panel.
-   */
-  hideShadow: PropTypes.bool,
-  /**
-   * Controls whether the panel can be closed by clicking on overlay.
-   */
-  isModal: PropTypes.bool,
-  /**
-   * Controls whether the panel is opened or not.
-   */
-  isOpen: PropTypes.bool.isRequired,
-  /**
-   * Left positioning property value in css lenght units
-   */
-  left: PropTypes.string,
-  /**
-   * Max width of the content
-   */
-  maxWidth: PropTypes.string,
-  /**
-   * Callback function fired when the Dialog is dismissed.
-   */
-  onDismiss: PropTypes.func,
-  /**
-   * Callback function fired when the Dialog is dismissed and action submitted.
-   */
-  onSubmit: PropTypes.func,
-  /**
-   * Right positioning property value in css lenght units
-   */
-  right: PropTypes.string,
-  /**
-   * Top positioning property value in css lenght units
-   */
-  top: PropTypes.string,
-  /**
-   * Transition style
-   */
-  transition: PropTypes.oneOf(['zoomIn', 'slideFromTop', 'slideFromBottom', 'fadeIn', 'none', 'slideFromLeft', 'slideFromRight']).isRequired,
-  /**
-   * Width of the content
-   */
-  width: PropTypes.string,
-  /**
-   * This number represents the zDepth of the panel.
-   */
-  zDepth: PropTypes.number,
-};
 
 export default Panel;
-
 
--- src/Panel/Panel.story.js
+++ src/Panel/Panel.story.js
@@ -3,10 +3,6 @@ import { storiesOf } from '@kadira/storybook';
 import Panel from './Panel';
 import PlaceholderDialogContent from '../../data/PlaceholderDialogContent';
 
-const FixedDiv = ({ zDepth }) => (
-  <div style={{position: 'fixed', zIndex: zDepth, top: 0, left: 0, bottom: 0, right: 0, background: 'hsla(301, 64%, 31%, 0.55)', color: 'white'}}>I am z-index: {zDepth}</div>
-);
-
 storiesOf('Panel', module)
   .add('left panel', () => (
     <Panel
@@ -20,38 +16,6 @@ storiesOf('Panel', module)
     >
       <PlaceholderDialogContent hasScroller={true} size="long" />
     </Panel>
-  ))
-  .add('z-index 12', () => (
-    <div>
-      <FixedDiv zDepth="12" />
-      <Panel
-        zDepth={10}
-        isOpen={true}
-        maxWidth={'320px'}
-        width={'93%'}
-        transition={'slideFromLeft'}
-        right={'auto'}
-        className={'color:white'}
-      >
-        <PlaceholderDialogContent hasScroller={true} size="long" />
-      </Panel>
-    </div>
-  ))
-  .add('z-index 5', () => (
-    <div>
-      <FixedDiv zDepth="5" />
-      <Panel
-        zDepth={10}
-        isOpen={true}
-        maxWidth={'320px'}
-        width={'93%'}
-        transition={'slideFromLeft'}
-        right={'auto'}
-        className={'color:white'}
-      >
-        <PlaceholderDialogContent hasScroller={true} size="long" />
-      </Panel>
-    </div>
   ))
   .add('right panel', () => (
     <Panel
